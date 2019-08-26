89ec6b3a49c3d03dce1ee85b294386f7f5285c4b
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2019-08-25 19:10:07 -0700

    commented out steps to code in game.js and added html element where I can display the computer-generated letter

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// computer generates random letter
+    // is an array needed here? if so, it will need a math.floor(math.random() * array.length)
+
+// user guesses a letter
+    // this will be a document.onkeyup event
+
+// user's guess is documented in the "guesses so far" line
+
+// guesses left decreases by 1
+
+// if the user's guess === the computer's guess then wins increases by 1
+
+// if guesses left reaches 0, losses increases by 1 and computer's letter is displayed
