a059fc5ce6d319b98dc3412358b601ba01c64acd
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2019-09-02 18:03:03 -0700

    changed how wins and losses were incremented as obj.textContent is a string not a number, made the reset function also clear out the guesses so far, and added some styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,66 @@
 // VARIABLES
 var wins = document.getElementById("wins");
+wins.textContent = 0;
 
 var losses = document.getElementById("losses");
+losses.textContent = 0;
 
 var guessesLeft = document.getElementById("guessesLeft");
+var guessCount = 10;
 
 var userGuesses = document.getElementById("userGuesses");
+var guessList = [];
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var isGameOver = false;
 
-// Assigning scores and guesses initial values
-wins.textContent = 0;
-
-losses.textContent = 0;
-
-guessesLeft.textContent = 10;
-
-// userGuesses.textContent = keystrokes.toString();
-
+// FUNCTIONS
 // Computer generates a random letter
 function generateLetter() {
     return computerLetter = letters[Math.floor(Math.random() * letters.length)];
 }
 
-generateLetter();
-console.log("computer letter: " + computerLetter);
-
+// game resets on win or loss
 function gameOver() {
-    
-    guessesLeft.textContent = 10;
-    
-    generateLetter();
-    console.log("new computer letter: " + computerLetter);
+    isGameOver = false
 
+    guessCount = 10
+    guessesLeft.textContent = guessCount;
+
+    guessList.length = 0;
+    userGuesses.textContent = guessList;
+
+    generateLetter();
 }
 
-// user guesses a letter, it is added to userGuesses element, and guessesLeft decreases by 1
-document.onkeyup = function (event) {
-    let userInput = event.key;
-    userGuesses.textContent += userInput;
-    guessesLeft.textContent--;
-    // console.log(losses.textContent);
+// ACTIVE CODE
+// call fxn to generate computer's letter
+generateLetter();
 
-    // if the user's guess === the computer's guess then wins increases by 1
+// user guesses a letter, it is added to userGuesses element, and guessesLeft decreases by 1
+document.onkeyup = function (event) { 
+    let userInput = event.key;
+    if (guessList.indexOf(userInput) === -1) {
+        guessList.push(userInput)
+        userGuesses.textContent = guessList;
+        guessCount--;
+        guessesLeft.textContent = guessCount;
+    } else {
+        alert("You already guessed '" + userInput + "'. Try another letter.")
+    }
+
+    // if the user's guess equals the computer's guess then wins increases by 1
     if (userInput === computerLetter) {
-        wins.textContent++;
+        wins.textContent = parseInt(wins.textContent) + 1;
+        var isGameOver = true;
+        console.log(typeof(parseInt(wins.textContent)));
+    } else if (guessesLeft.textContent < 1) {
+        losses.textContent = parseInt(losses.textContent) + 1;
         var isGameOver = true;
     }
-    else if (guessesLeft.textContent < 1) {
-        losses.textContent++;
-        var isGameOver = true;
-    }
-    console.log(isGameOver);
+    
     if (isGameOver) {
         gameOver();
     }
 }
-
